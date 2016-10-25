5e184ec70f71b54cb540047ef8055ebd31737573
Surya Avala <suryaavala.dev@gmail.com>
2016-10-25 23:05:11 +1100

    edge defined and graph under definition

--- ass2/graph.py
+++ ass2/graph.py
@@ -6,6 +6,9 @@
 #Class for implementation of Graphs
 
 class Node:
+    '''
+    Node object hold the name of the Node
+    '''
     def __init__(self,name):
         '''
         Name is a string with the name of the node
@@ -23,3 +26,50 @@ class Node:
         Prints name
         '''
         return self.name
+
+class Edge:
+    '''
+    Assumes src, dst are nodes
+    Edge from src->dst
+    '''
+    def __init__(self,src,dst):
+        self.src = src
+        self.dst = dst
+
+    def getSource(self):
+        return self.src
+
+    def getDestination(self):
+        return self.dst
+
+    def __str__(self):
+        return self.src.getName() + '->' + self.dst.getName()
+
+
+
+class Graph:
+    '''
+    Edge is a dict with key as node name and value as a list of adj nodes
+    '''
+
+    def __init__(self):
+        '''
+        Initiates dict edges
+        '''
+        self.edges = {}
+
+    def addNode(self, node):
+        if node in self.edges:
+            raise ValueError ("Duplicate Error")
+        else:
+            self.edges[node] = []
+
+    def addEdge(self, node, edge):
+        pass
+
+
+if __name__ == "__main__":
+    src = Node('src')
+    dst = Node('dst')
+    e = Edge(src,dst)
+    print(e)
