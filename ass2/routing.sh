8b4ff8cdccb20fd42b28fdf29ffc04ae43882ad4
Surya Avala <suryaavala.dev@gmail.com>
2016-10-26 19:52:52 +1100

    routing.sh automated script for transmitting shit

---
+++ ass2/routing.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+python3 Lsr.py A 2000 ./Topology1/configA.txt > ./output/outputA.txt &
+
+
+python3 Lsr.py B 2001 ./Topology1/configB.txt > ./output/outputB.txt &
+
+
+python3 Lsr.py C 2002 ./Topology1/configC.txt > ./output/outputC.txt &
+
+python3 Lsr.py D 2003 ./Topology1/configD.txt > ./output/outputD.txt &
+
+python3 Lsr.py E 2004 ./Topology1/configE.txt > ./output/outputE.txt &
+
+python3 Lsr.py F 2005 ./Topology1/configF.txt > ./output/outputF.txt &
+
+# python3 Lsr.py A 2000 ./Topology1/configA.txt &
+#
+#
+# python3 Lsr.py B 2001 ./Topology1/configB.txt &
+#
+#
+# python3 Lsr.py C 2002 ./Topology1/configC.txt &
+#
+# python3 Lsr.py D 2003 ./Topology1/configD.txt &
+#
+# python3 Lsr.py E 2004 ./Topology1/configE.txt &
+#
+# python3 Lsr.py F 2005 ./Topology1/configF.txt  &
