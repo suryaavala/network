914003f6c46463f201e8986f6b1c502d57a39ec3
Surya Avala <suryaavala.dev@gmail.com>
2016-10-26 14:42:11 +1100

    initialised Lsr.py

---
+++ ass2/Lsr.py
@@ -0,0 +1,8 @@
+#Surya Avinash Avala, z5096886
+#PYTHON 3 ONLY, doesn't work with Python2
+#Requires graph.py file
+#hosted on bitbucket as a private repo, will be made public after one week on the assignment deadline
+#git@bitbucket.org:saavala2/network.git
+#Requires dijkstra.py and graph.py files to run
+
+#Lsr.py
